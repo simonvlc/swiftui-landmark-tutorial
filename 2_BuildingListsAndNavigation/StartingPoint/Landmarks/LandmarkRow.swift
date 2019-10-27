3e98ab904fc43a18b5a4a88ecb1a94eb59bb117b
Simón Muñoz <simonvlc@gmail.com>
2019-10-27 15:52:17 +0100

    adding navigation view

---
+++ 2_BuildingListsAndNavigation/StartingPoint/Landmarks/LandmarkRow.swift
@@ -0,0 +1,28 @@
+//
+//  LandmarkRow.swift
+//  Landmarks
+//
+//  Created by Simón Muñoz on 27/10/2019.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var landmark: Landmark
+
+    var body: some View {
+        HStack {
+            landmark.image
+                .resizable()
+                .frame(width: 50, height: 50)
+            Text(landmark.name)
+        }
+    }
+}
+
+struct LandmarkRow_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkRow(landmark: landmarkData[0])
+    }
+}
---
+++ 2_BuildingListsAndNavigation/StartingPoint/Landmarks/Landmarks/LandmarkList.swift
@@ -0,0 +1,28 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Simón Muñoz on 27/10/2019.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        NavigationView {
+            List(landmarkData) { landmark in
+                NavigationLink(destination: LandmarkDetail()) {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationBarTitle(Text("Landmarks"))
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
